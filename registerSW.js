13e7b2c319c0af4e13ee8ea5b0644108a8c47bfd
Patrick Ziegler <patrickziegler@Patrick-Ziegler-MBP-13.local>
2026-05-29 20:16:45 +0200

    Deploy: rebuild with correct base and 404.html

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ChinesischApp/sw.js', { scope: '/ChinesischApp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
